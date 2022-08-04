5b32c018400b844334e6c1e397885767946e47ef
ParkGiBeom97 <rlqja0509@naver.com>
2022-08-04 20:17:09 +0900

    링크및 상세페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
 import MainPageComponent from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from './upload';
+import ProductPage from './product';
 
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <div>
+        <div id="header">
+          <div id="header-area">
+            <img src="/images/icons/logo.png" />
+          </div>
+        </div>
+        <div id="body">
+          <switch>
+            <Route exact={true} path={"/"}>
+              <MainPageComponent />
+            </Route>
+            <Route exact={true} path={"/product/:id"}>
+              <ProductPage />
+            </Route>
+            <Route exact={true} path={"/upload"}>
+              <UploadPage />
+            </Route>
+          </switch>
+        </div>
+        <div id="footer"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/product/index.js
@@ -0,0 +1,47 @@
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import './index.css';
+
+function ProductPage() {
+
+    const {id} = useParams();
+
+    const[product, setProducts] = useState(null);
+
+    useEffect(function(){
+        axios.get(`https://62162e65-592c-414f-abc4-6e9ef432a34a.mock.pstmn.io/products/${id}`).then(
+            function(result){
+                setProducts(result.data);
+                console.log(result);
+            }
+        ).catch(function(error){
+            console.log(error);
+        });
+    },[]);
+
+    if(product === null) {
+        return<h1>상품 정보를 받고 있습니다....</h1>
+    }
+
+    return(
+        <div>
+            <div id="image-box">
+                <img src={"/"+product.imageUrl} />
+            </div>
+            <div id="profile-box">
+                <img src="/images/icons/avatar.png" />
+                <span>{product.seller}</span>
+            </div>
+            <div id = "contents-box">
+                <div id = "name">{product.name}</div>
+                <div id = "price">{product.price}원</div>
+                <div id = "createdAt">2022년 8월 4일</div>
+                <div id = "description">{product.description}</div>
+            </div>
+        </div>
+    );
+
+}
+
+export default ProductPage;
